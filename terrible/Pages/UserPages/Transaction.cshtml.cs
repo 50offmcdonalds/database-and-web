e70fa35bf8cf707a703e9c22e7245384bc3edde5
STEVE REDMILE <c0023341@hallam.shu.ac.uk>
2020-12-07 17:15:34 +0000

    Revert "create transactions table"
    This reverts commit 6b6d6b3d6b3521c9336b18d3c4ed8e683f08a14f.

--- terrible/Pages/UserPages/Transaction.cshtml.cs
+++ terrible/Pages/UserPages/Transaction.cshtml.cs
@@ -75,8 +75,7 @@ namespace terrible.Pages.UserPages
                 
                 command.Connection = conn;
                 command.CommandText = @"UPDATE [User] SET [Balance] = [Balance] + @NewBalance WHERE [Id] = @TransferID;
-                                        UPDATE [User] SET [Balance] = [Balance] - @NewBalance WHERE [Id] = @SenderID;
-                                        INSERT INTO[Transactions] ([SenderID], [ReceiverID], [Amount]) VALUES(@SenderID, @TransferID, @NewBalance)";
+                                        UPDATE [User] SET [Balance] = [Balance] - @NewBalance WHERE [Id] = @SenderID";
 
                 command.Parameters.AddWithValue("@NewBalance", Transaction.TransferAmount);
                 command.Parameters.AddWithValue("@TransferID", Transaction.ReceiverID);
