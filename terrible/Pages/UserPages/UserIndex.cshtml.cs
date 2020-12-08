4a5f985c1043d49bc5ac4d62feca110ec3be285c
STEVE REDMILE <c0023341@hallam.shu.ac.uk>
2020-12-08 13:59:40 +0000

    add admin panel button to user page -admin panel button is only visible to admin accounts

--- terrible/Pages/UserPages/UserIndex.cshtml.cs
+++ terrible/Pages/UserPages/UserIndex.cshtml.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using terrible.Models;
+
 
 namespace terrible.Pages.UserPages
 {
     public class UserIndexModel : PageModel
     {
+
         public int? UserID;
         public const string SessionKeyName1 = "userID";
 
@@ -19,11 +22,14 @@ namespace terrible.Pages.UserPages
         public string Name;
         public const string SessionKeyName3 = "name";
 
+        public bool Admin;
+        public const string SessionKeyName4 = "admin";
+
         public decimal Balance;
-        public const string SessionKeyName4 = "balance";
+        public const string SessionKeyName5 = "balance";
 
         public string SessionID;
-        public const string SessionKeyName5 = "sessionID";
+        public const string SessionKeyName6 = "sessionID";
 
         
         public IActionResult OnGet()
@@ -31,9 +37,9 @@ namespace terrible.Pages.UserPages
             UserID = HttpContext.Session.GetInt32(SessionKeyName1);
             Username = HttpContext.Session.GetString(SessionKeyName2);
             Name = HttpContext.Session.GetString(SessionKeyName3);
-            Balance = decimal.Round(Convert.ToDecimal(HttpContext.Session.GetString(SessionKeyName4)),2);
-            SessionID = HttpContext.Session.GetString(SessionKeyName5);
-
+            Admin = Convert.ToBoolean(HttpContext.Session.GetString(SessionKeyName4));
+            Balance = decimal.Round(Convert.ToDecimal(HttpContext.Session.GetString(SessionKeyName5)),2);
+            SessionID = HttpContext.Session.GetString(SessionKeyName6);
 
             if (string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(SessionID))
             {
